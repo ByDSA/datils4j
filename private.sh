daabe91ed05a4aed0e8a6986dd091fa833505309
danisales <danisales.es@gmail.com>
2018-11-17 19:39:16 +0100

    commit33

--- private.sh
+++
@@ -1,8 +0,0 @@
-git config --global user.name "danisales"
-git config --global user.email "danisales.es@gmail.com"
-git rm --cached -r ./
-#echo "/bin/" > .gitignore
-git add --all
-git commit -m "commit3"
-git push master
-git push -u private master
