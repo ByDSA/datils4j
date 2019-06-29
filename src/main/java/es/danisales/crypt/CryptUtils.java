9135c18ac3d95380090dfa2d93664ec620eb5adf
danisales <danisales.es@gmail.com>
2019-06-29 23:52:39 +0200

    Supress Warnings and extend crypt algorithms

--- src/main/java/es/danisales/crypt/CryptUtils.java
+++ src/main/java/es/danisales/crypt/CryptUtils.java
@@ -8,26 +8,23 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class CryptUtils {
-	public static final String SHA256 = "SHA-256";
-	
-	public static byte[] hashFile(File f, String alg) throws NoSuchAlgorithmException, IOException {
+	@SuppressWarnings("WeakerAccess")
+	public static byte[] hashFile(File f, HashingAlgorithm alg) throws NoSuchAlgorithmException, IOException {
 		byte[] buffer= new byte[8192];
 		int count;
-		MessageDigest digest = MessageDigest.getInstance(alg);
+		MessageDigest digest = MessageDigest.getInstance(alg.value());
 		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
 		while ((count = bis.read(buffer)) > 0) {
 			digest.update(buffer, 0, count);
 		}
 		bis.close();
 
-		byte[] hash = digest.digest();
-
-		return hash;
+		return digest.digest();
 	}
 
 	public static byte[] hashFileSHA256(File f) throws IOException {
 		try {
-			return hashFile(f, SHA256);
+			return hashFile(f, HashingAlgorithm.SHA256);
 		} catch ( NoSuchAlgorithmException e ) {
 			e.printStackTrace();
 			System.exit( -1 );
---
+++ src/main/java/es/danisales/crypt/HashingAlgorithm.java
@@ -0,0 +1,18 @@
+package es.danisales.crypt;
+
+import es.danisales.enums.EnumValue;
+
+public enum HashingAlgorithm implements EnumValue<String> {
+    SHA256("SHA-256"), MD2("MD2"), MD5("MD5"), SHA1("SHA-1"), SHA384("SHA-384"), SHA512("SHA-512");
+
+    String val;
+
+    HashingAlgorithm(String v) {
+        val = v;
+    }
+
+    @Override
+    public String value() {
+        return val;
+    }
+}
