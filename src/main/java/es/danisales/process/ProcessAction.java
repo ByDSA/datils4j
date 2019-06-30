96a494d6ea795e549c628e962f50e9983e5ee262
danisales <danisales.es@gmail.com>
2019-06-30 15:58:06 +0200

    Supress Warnings and modularize output

--- src/main/java/es/danisales/process/ProcessAction.java
+++ src/main/java/es/danisales/process/ProcessAction.java
@@ -10,14 +10,17 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class ProcessAction extends Action {
+    @SuppressWarnings("WeakerAccess")
     String[] paramsWithName;
 
+    @SuppressWarnings("unused")
     public ProcessAction(String fname, List<String> params) {
         super(Mode.CONCURRENT);
-        String[] paramsArray = params.toArray(new String[params.size()]);
+        String[] paramsArray = params.toArray(new String[0]);
         constructor(fname, paramsArray);
     }
 
+    @SuppressWarnings("unused")
     public ProcessAction(String fname, String... params) {
         super(Mode.CONCURRENT);
 
@@ -31,14 +34,11 @@ public class ProcessAction extends Action {
     protected void constructor(String fname, String... params) {
         paramsWithName = new String[ params.length +1 ];
         paramsWithName[0] = fname;
-        for (int i = 1; i <= params.length; i++) {
-            paramsWithName[i] = params[i-1];
-        }
+        System.arraycopy(params, 0, paramsWithName, 1, params.length);
     }
 
     @Override
     protected void innerRun() {
-        int result = 1;
         try {
             if (paramsWithName == null || paramsWithName.length == 0)
                 throw new NoArgumentsException();
@@ -53,7 +53,7 @@ public class ProcessAction extends Action {
             Thread normalOutputThread = startNormalOutputListener(p);
             startErrorOutputListener(p);
 
-            result = p.waitFor();
+            int result = p.waitFor();
             normalOutputThread.join();
             if (result != 0) {
                 onError(result);
@@ -67,6 +67,7 @@ public class ProcessAction extends Action {
         }
     }
 
+    @SuppressWarnings("WeakerAccess")
     public void onNoArguments(String[] paramsWithName) {
         System.err.println("no valid arguments");
         if (paramsWithName != null)
@@ -74,10 +75,12 @@ public class ProcessAction extends Action {
             System.err.println(str);
     }
 
+    @SuppressWarnings("WeakerAccess")
     public void onInterrupted() {
         System.err.println("procccess interrupted");
     }
 
+    @SuppressWarnings("WeakerAccess")
     public void onNotFound() {
         System.err.println("procccess not found");
     }
@@ -91,11 +94,11 @@ public class ProcessAction extends Action {
                                 (new InputStreamReader(p.getInputStream()));
 
                 while ((line = input.readLine()) != null)
-                    Logging.log(line);
+                    onOutLine(line);
 
 
                 input.close();
-            } catch(Exception e) {}
+            } catch(Exception ignored) {}
         });
 
         normalOutputThread.start();
@@ -103,7 +106,7 @@ public class ProcessAction extends Action {
         return normalOutputThread;
     }
 
-    private Thread startErrorOutputListener(Process p) {
+    private void startErrorOutputListener(Process p) {
         Thread errorOutputThread = new Thread(() -> {
             try {
                 String line;
@@ -112,23 +115,33 @@ public class ProcessAction extends Action {
                                 (new InputStreamReader(p.getErrorStream()));
 
                 while ((line = input.readLine()) != null)
-                    Logging.error(line);
+                    onErrLine(line);
 
 
                 input.close();
-            } catch(Exception e) {}
+            } catch(Exception ignored) {}
         });
 
         errorOutputThread.start();
-
-        return errorOutputThread;
     }
 
+    @SuppressWarnings("WeakerAccess")
     public void onError(int code) {
         Logging.error("Process failed with status: " + code);
     }
 
-    public class NoArgumentsException extends RuntimeException {
+    @SuppressWarnings("WeakerAccess")
+    public void onOutLine(String line) {
+        Logging.log(line);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public void onErrLine(String line) {
+        Logging.error(line);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public static class NoArgumentsException extends RuntimeException {
         public NoArgumentsException() {
             super("No arguments");
         }
