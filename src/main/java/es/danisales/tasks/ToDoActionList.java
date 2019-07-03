1c8ec36252ecbf25a5c78043c236498ea7288eb8
danisales <danisales.es@gmail.com>
2019-07-03 23:58:49 +0200

    Add ToDoActionList

---
+++ src/main/java/es/danisales/tasks/ToDoActionList.java
@@ -0,0 +1,28 @@
+package es.danisales.tasks;
+
+import es.danisales.log.string.Logging;
+
+class ToDoActionList extends ActionList {
+    public ToDoActionList() {
+        super(Mode.CONCURRENT);
+    }
+    @Override
+    public boolean add(Action a) {
+        boolean ret = super.add(a);
+
+        a.addAfter(() -> {
+            Logging.log("End remove ToDoActionList " + a);
+            remove(a);
+        });
+        a.addAtInterruptActions(() -> {
+            Logging.log("Interrupt " + a);
+            interrupt();
+        });
+
+        if (!isRunning())
+            run();
+
+        return ret;
+    }
+
+}
