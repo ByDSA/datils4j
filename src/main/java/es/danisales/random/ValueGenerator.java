e3b5d5e222a2bea348a04821fe458fd2783db9bc
danisales <danisales.es@gmail.com>
2019-07-14 05:13:25 +0200

    set as noninstantiable

--- src/main/java/es/danisales/random/ValueGenerator.java
+++ src/main/java/es/danisales/random/ValueGenerator.java
@@ -3,8 +3,12 @@ package es.danisales.random;
 import java.util.Random;
 
 public class ValueGenerator {
+    static Random rd = new Random(); // creating ValueGenerator object
+
+    private ValueGenerator() {
+    } // noninstantiable
+
     public static int[] integerArray(final int size) {
-        Random rd = new Random(); // creating ValueGenerator object
         int[] arr = new int[size];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rd.nextInt(); // storing random integers in an array
@@ -14,7 +18,6 @@ public class ValueGenerator {
     }
 
     public static long[] longArray(final int size) {
-        Random rd = new Random(); // creating ValueGenerator object
         long[] arr = new long[size];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rd.nextLong(); // storing random integers in an array
