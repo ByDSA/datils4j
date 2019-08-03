9a8cfcd1e3054e7a2eac9d240416e6a0f220eee2
danisales <danisales.es@gmail.com>
2019-08-04 01:48:54 +0200

    Remove warnings

--- src/main/java/es/danisales/datastructures/ListWrapper.java
+++ src/main/java/es/danisales/datastructures/ListWrapper.java
@@ -1,139 +1,142 @@
 package es.danisales.datastructures;
 
-import java.util.ArrayList;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ListWrapper<T> implements List<T> {
-	List<T> list;
-	
-	protected ListWrapper(List<T> l) {
-		list = l;
-	}
-	
-	public static <U> ListWrapper<U> newArrayList() {
-		return new ListWrapper<>( new ArrayList<U>() );
-	}
-	
-	public static <U> ListWrapper<U> newCopyOnWriteArrayList() {
-		return new ListWrapper<>( new CopyOnWriteArrayList<U>() );
+    private final List<T> listAdapter;
+
+    private ListWrapper(List<T> listAdapter) {
+        this.listAdapter = listAdapter;
+    }
+
+    public static <T> ListWrapper of(List<T> l) {
+        return new ListWrapper<>(l);
 	}
 
 	@Override
 	public boolean add(T e) {
-		return list.add( e );
+        return listAdapter.add(e);
 	}
 
 	@Override
 	public void add(int index, T element) {
-		list.add( index, element );
+        listAdapter.add(index, element);
 	}
 
 	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		return list.addAll( c );
+    public boolean addAll(@NonNull Collection<? extends T> c) {
+        return listAdapter.addAll(c);
 	}
 
 	@Override
-	public boolean addAll(int index, Collection<? extends T> c) {
-		return list.addAll( c );
+    public boolean addAll(int index, @NonNull Collection<? extends T> c) {
+        return listAdapter.addAll(c);
 	}
 
 	@Override
 	public void clear() {
-		list.clear();
+        listAdapter.clear();
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		return list.contains( o );
+        return listAdapter.contains(o);
 	}
 
 	@Override
-	public boolean containsAll(Collection<?> c) {
-		return list.containsAll( c );
+    public boolean containsAll(@NonNull Collection<?> c) {
+        return listAdapter.containsAll(c);
 	}
 
 	@Override
 	public T get(int index) {
-		return list.get( index );
+        return listAdapter.get(index);
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		return list.indexOf( o );
+        return listAdapter.indexOf(o);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return list.isEmpty();
+        return listAdapter.isEmpty();
 	}
 
 	@Override
+    @NonNull
 	public Iterator<T> iterator() {
-		return list.iterator();
+        return listAdapter.iterator();
 	}
 
 	@Override
 	public int lastIndexOf(Object o) {
-		return list.lastIndexOf( o );
+        return listAdapter.lastIndexOf(o);
 	}
 
 	@Override
+    @NonNull
 	public ListIterator<T> listIterator() {
-		return list.listIterator();
+        return listAdapter.listIterator();
 	}
 
 	@Override
+    @NonNull
 	public ListIterator<T> listIterator(int index) {
-		return list.listIterator( index );
+        return listAdapter.listIterator(index);
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		return list.remove( o );
+        return listAdapter.remove(o);
 	}
 
 	@Override
 	public T remove(int index) {
-		return list.remove( index );
+        return listAdapter.remove(index);
 	}
 
 	@Override
-	public boolean removeAll(Collection<?> c) {
-		return list.removeAll( c );
+    public boolean removeAll(@NonNull Collection<?> c) {
+        return listAdapter.removeAll(c);
 	}
 
 	@Override
-	public boolean retainAll(Collection<?> c) {
-		return list.retainAll( c );
+    public boolean retainAll(@NonNull Collection<?> c) {
+        return listAdapter.retainAll(c);
 	}
 
 	@Override
 	public T set(int index, T element) {
-		return list.set( index, element );
+        return listAdapter.set(index, element);
 	}
 
 	@Override
 	public int size() {
-		return list.size();
+        return listAdapter.size();
 	}
 
 	@Override
+    @NonNull
 	public List<T> subList(int fromIndex, int toIndex) {
-		return list.subList( fromIndex, toIndex );
+        return listAdapter.subList(fromIndex, toIndex);
 	}
 
 	@Override
+    @NonNull
 	public Object[] toArray() {
-		return list.toArray();
-	}
+        return listAdapter.toArray();
+    }
 
-	@Override
-	public <T> T[] toArray(T[] a) {
-		return list.toArray( a );
-	}
+    @SuppressWarnings("SuspiciousToArrayCall")
+    @Override
+    @NonNull
+    public <TT> TT[] toArray(@NonNull TT[] a) {
+        return listAdapter.toArray(a);
+    }
 }
