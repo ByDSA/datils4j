3332f6c708a7a5fecaf98e1e1b3a3f3748343004
danisales <danisales.es@gmail.com>
2019-08-15 19:23:25 +0200

    Add new tests

--- src/test/java/es/danisales/tasks/ToDoActionListTest.java
+++ src/test/java/es/danisales/tasks/ToDoActionListTest.java
@@ -8,36 +8,79 @@ import static es.danisales.time.Sleep.sleep;
 import static org.junit.Assert.*;
 
 public class ToDoActionListTest {
-    @Test(timeout = 20000)
-    public void testAutoRun() {
+    private Action createTaskIncrementor(Action.Mode mode, AtomicInteger ai) {
+        Action a = Action.of(mode, (Action ac) -> ai.incrementAndGet());
+        a.setName("IncrementorAction");
+        return a;
+    }
+
+    private ToDoActionList createTodoList() {
         ToDoActionList td = new ToDoActionList();
         td.setName("ToDoList");
-        AtomicInteger ai = new AtomicInteger(0);
+        return td;
+    }
 
-        Action a = Action.of(Action.Mode.SEQUENTIAL, (Action ac) -> ai.incrementAndGet());
-        a.setName("IncrementorAction");
+    @Test
+    public void initialState() {
+        ToDoActionList td = createTodoList();
+
         assertFalse(td.isRunning());
         assertFalse(td.isReady());
         assertEquals(0, td.size());
+    }
 
+    @Test
+    public void runWithoutActionsState() {
+        ToDoActionList td = createTodoList();
         td.run();
         sleep(50);
         assertFalse(td.isReady());
         assertFalse(td.isDone());
         assertTrue(td.isRunning());
-        assertEquals(0, ai.get());
+        assertEquals(0, td.size());
+    }
 
+    @Test
+    public void autorun() {
+        ToDoActionList td = createTodoList();
+        AtomicInteger ai = new AtomicInteger(0);
+
+        Action a = createTaskIncrementor(Action.Mode.SEQUENTIAL, ai);
+
+        td.run();
         td.add(a);
         td.waitFor();
         assertEquals(1, ai.get());
         assertEquals(0, td.size());
         assertTrue(td.isRunning());
         assertFalse(td.isDone());
+    }
 
-        Action a2 = Action.of(Action.Mode.SEQUENTIAL, (Action ac) -> ai.incrementAndGet());
-        td.add(a2);
+    // todo: move to ActionList
+    @Test(timeout = 20000, expected = ActionList.AddedException.class)
+    public void addSameActionTwice() {
+        ToDoActionList td = createTodoList();
+        AtomicInteger ai = new AtomicInteger(0);
+
+        Action a = createTaskIncrementor(Action.Mode.SEQUENTIAL, ai);
+        td.add(a);
+        td.add(a);
+
+        td.run();
         td.waitFor();
-        assertEquals(2, ai.get());
+    }
+
+    @Test(timeout = 20000)
+    public void addBeforeRun() {
+        ToDoActionList td = createTodoList();
+        AtomicInteger ai = new AtomicInteger(0);
+
+        Action a = createTaskIncrementor(Action.Mode.SEQUENTIAL, ai);
+        td.add(a);
+        td.run();
+        td.waitFor();
+
+        assertEquals(1, ai.get());
         assertEquals(0, td.size());
         assertTrue(td.isRunning());
         assertFalse(td.isDone());
