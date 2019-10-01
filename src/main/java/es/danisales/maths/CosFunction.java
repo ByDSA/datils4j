a6690c21912b3f8fb2bf4c06f7f9e32f9ca1ba10
danisales <danisales.es@gmail.com>
2019-10-01 14:04:54 +0200

    Add getters and setters

--- src/main/java/es/danisales/maths/CosFunction.java
+++ src/main/java/es/danisales/maths/CosFunction.java
@@ -6,7 +6,10 @@ import java.util.function.Function;
 public class CosFunction implements Function<Double, Double> {
 	double period;
 	long length;
-	
+
+	private Object lengthLock = new Object();
+	private Object periodLock = new Object();
+
 	public CosFunction(double T, long l) {
 		period = T;
 		length = l;
@@ -14,6 +17,35 @@ public class CosFunction implements Function<Double, Double> {
 
 	@Override
 	public Double apply(Double x) {
-		return (Math.cos(x*length/(period)* Math.PI)+1)/2 * 127;
+		synchronized (lengthLock) {
+			synchronized (periodLock) {
+				return (Math.cos(x * length / (period) * Math.PI) + 1) / 2 * 127;
+			}
+		}
+	}
+
+	public long getLength() {
+		synchronized (lengthLock) {
+			return length;
+		}
+	}
+
+	public void setLength(long length) {
+		synchronized (lengthLock) {
+			this.length = length;
+		}
+	}
+
+	public double getPeriod() {
+		synchronized (periodLock) {
+			return period;
+		}
+	}
+
+	public void setPeriod(double period) {
+		synchronized (periodLock) {
+			this.period = period;
+		}
 	}
 }
+
