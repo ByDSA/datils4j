7b1938ec18d8a1509bf63ec87edc5d4226c36d50
danisales <danisales.es@gmail.com>
2019-10-11 16:29:07 +0200

    Remove Line class

--- src/main/java/es/danisales/io/text/csv/CsvFileKeyValueString.java
+++ src/main/java/es/danisales/io/text/csv/CsvFileKeyValueString.java
@@ -3,21 +3,13 @@ package es.danisales.io.text.csv;
 import java.io.File;
 
 @SuppressWarnings("unused")
-public class CsvFileKeyValueString extends CsvFile<String, CsvFileKeyValueString.Line> {
+public class CsvFileKeyValueString extends CsvFile<String, KeyValueLine<String, String>> {
     public CsvFileKeyValueString(File file) {
         super(file);
     }
 
     @Override
-    protected Line readLine(String[] e) {
-        return new Line(e[0], e[1]);
-    }
-
-
-    @SuppressWarnings("WeakerAccess")
-    public static class Line extends KeyValueLine<String, String> {
-        Line(String key, String value) {
-            super(key, value);
-        }
+    protected KeyValueLine<String, String> readLine(String[] e) {
+        return new KeyValueLine<>(e[0], e[1]);
     }
 }
