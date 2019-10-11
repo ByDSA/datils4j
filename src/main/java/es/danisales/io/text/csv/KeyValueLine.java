634c4e77c97f8e16efa633139798a9ba8520b0dc
danisales <danisales.es@gmail.com>
2019-10-11 13:50:01 +0200

    Add Pair class

--- src/main/java/es/danisales/io/text/csv/KeyValueLine.java
+++ src/main/java/es/danisales/io/text/csv/KeyValueLine.java
@@ -1,7 +1,7 @@
 package es.danisales.io.text.csv;
 
 import es.danisales.others.Keyable;
-import javafx.util.Pair;
+import es.danisales.utils.Pair;
 
 class KeyValueLine<K, V> extends Pair<K, V> implements Keyable<K> {
     KeyValueLine(K key, V v) {
