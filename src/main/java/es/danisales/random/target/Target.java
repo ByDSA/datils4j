5b6643f20b0e17f2bc81e640b9394b04083977c8
danisales <danisales.es@gmail.com>
2019-10-14 20:48:43 +0200

    Add pick(int n) (multiple picker at once)

--- src/main/java/es/danisales/random/target/Target.java
+++ src/main/java/es/danisales/random/target/Target.java
@@ -1,8 +1,18 @@
 package es.danisales.random.target;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Target<RET> {
 	RET pickDart(long dart);
 	RET pick();
+
+    default List<RET> pick(int n) {
+        List<RET> ret = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            ret.add(pick());
+        return ret;
+    }
 	long getSurface();
 	void next();
 }
