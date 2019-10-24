c1ae30efddd8abbdee3a88c54e0224956d76c5a4
danisales <danisales.es@gmail.com>
2019-10-24 02:36:29 +0200

    Add lineToString method

--- src/main/java/es/danisales/io/text/TextFile.java
+++ src/main/java/es/danisales/io/text/TextFile.java
@@ -13,7 +13,12 @@ public class TextFile extends LinearStringFile<String> {
 	protected String stringToLine(long n, String l) {
 		return l;
 	}
-	
+
+    @Override
+    protected String lineToString(long i, String s) {
+        return s;
+    }
+
 	@Override
 	public boolean save() {
 		Path path = toPath();
--- src/main/java/es/danisales/io/text/csv/CsvFileKeyValueString.java
+++ src/main/java/es/danisales/io/text/csv/CsvFileKeyValueString.java
@@ -1,11 +1,17 @@
 package es.danisales.io.text.csv;
 
+import java.io.File;
 import java.nio.file.Path;
 
 @SuppressWarnings("unused")
 public class CsvFileKeyValueString extends CsvFile<String, KeyValueLine<String, String>> {
     public CsvFileKeyValueString(Path path) {
         super(path);
+    }
+
+    @Override
+    protected String lineToString(long i, KeyValueLine<String, String> l) {
+        return l.getKey() + File.separator + l.getValue();
     }
 
     @Override
