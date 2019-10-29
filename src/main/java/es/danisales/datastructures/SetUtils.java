163f046b696a5f3e62c53a866bfda60de5c9fd20
danisales <danisales.es@gmail.com>
2019-10-29 01:14:22 +0100

    Use List instead "..."

--- src/main/java/es/danisales/datastructures/SetUtils.java
+++ src/main/java/es/danisales/datastructures/SetUtils.java
@@ -1,17 +1,23 @@
 package es.danisales.datastructures;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import java.util.EnumSet;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 @SuppressWarnings("unused")
 public class SetUtils {
 	private SetUtils() {
 	} // noninstantiable
 
-	@SuppressWarnings("all")
-	public static <E extends Enum<E>> EnumSet<E> concat(EnumSet<E>... sets) {
-		EnumSet<E> ret = EnumSet.copyOf( sets[0] );
-		for (int i = 1; i < sets.length; i++)
-			ret.addAll( sets[i] );
+    public static <E extends Enum<E>> EnumSet<E> concat(@NonNull List<EnumSet<E>> sets) {
+        checkArgument(sets.size() > 0);
+
+        EnumSet<E> ret = EnumSet.copyOf(sets.get(0));
+        for (int i = 1; i < sets.size(); i++)
+            ret.addAll(sets.get(i));
 		
 		return ret;
 	}
